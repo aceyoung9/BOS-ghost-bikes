6fe008c28f43d57b63517aac0986683091817ff8
Ace Young <ayoung@tankdesign.com>
2019-10-19 21:54:55 -0400

    Add Bootstrap, SCSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import "../sass/styles.scss"
+
+export default () => (
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-8 my-3">
+        <h1>Boston-area Ghost Bikes</h1>
+      </div>
+    </div>
+  </div>
+)
